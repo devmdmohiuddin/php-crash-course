7fbd2b015eea6cdf20e625fc50ff6b322de609a8
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-26 13:45:53 +0600

    feat: output

---
+++ 01.output.php
@@ -0,0 +1,17 @@
+<?php 
+// echo
+echo "Hello World", " mohi"; // multiple value accepted
+
+// print
+print "Zakaria"; // multitple value not accepted
+
+// print_r()
+print_r([1, 2, 3]); // basically for array output
+
+// var_dump()
+var_dump("Hello"); // datatype return & also value
+
+// var_export()
+var_export(5); // similar to var_dump()
+
+?>
