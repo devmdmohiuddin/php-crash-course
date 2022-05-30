2ab9bb3a6572b49be35278eeb2dd5d82c5ead70c
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-30 13:41:53 +0600

    feat: super globals

---
+++ 09_super-globals.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ul>
+    <li>$GLOBALS</li>
+    <li>$_SERVER</li>
+    <li>$_REQUEST</li>
+    <li>$_POST</li>
+    <li>$_GET</li>
+    <li>$_FILES</li>
+    <li>$_ENV</li>
+    <li>$_COOKIE</li>
+    <li>$_SESSION</li>
+  </ul>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
